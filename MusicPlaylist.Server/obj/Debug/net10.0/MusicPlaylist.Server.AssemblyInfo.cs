21f8fd07d49cdfbcfde27f931c441d0248634d95
yves shema <yshema@bcit.ca>
2026-04-09 17:10:25 -0700

    update ci-cd workflow

--- MusicPlaylist.Server/obj/Debug/net10.0/MusicPlaylist.Server.AssemblyInfo.cs
+++ MusicPlaylist.Server/obj/Debug/net10.0/MusicPlaylist.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicPlaylist.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44c97b33a05dd2596b3e64f715136f92e2509c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a7b764970f085a4fc955bad44c2d57c964d9a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicPlaylist.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicPlaylist.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
